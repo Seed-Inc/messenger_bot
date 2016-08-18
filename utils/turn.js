d5bee849f5de39d0de47a6a3528513290f62bd90
Zac Rolland <zrolland@gmail.com>
2016-08-17 17:01:09 -0700

    added turn logger

---
+++ utils/turn.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const allTurns = require('../content').CONVERSATION_TURNS_LIST;
+let currentTurn = allTurns[0].name;
+
+
+/*
+ * Collection of methods related to identifying where the user 'is' in the
+ * flow of conversation with the bot
+ */
+module.exports = {
+
+  set(turn) {
+    currentTurn = turn;
+  },
+
+  get() {
+    console.log("THE CURRENT TURN IS: " + currentTurn)
+    return currentTurn;
+  },
+
+  goTo() {
+
+  }
+
+};
